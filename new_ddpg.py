024488d5ca73e8609253b83c549de3c3b98d4ce9
baibizhe <33809209+baibizhe@users.noreply.github.com>
2020-07-23 04:13:19 -0400

    new_ddpg.py change

--- new_ddpg.py
+++ new_ddpg.py
@@ -178,78 +178,9 @@ import random
 import collections
 import numpy as np
 
-def run_episode(env, agent, rpm):
-    fff=0
-
-    obs = env.reset()
-    total_reward, steps = 0, 0
-    while True:
-        steps += 1
-        fff=fff+1
-        batch_obs = np.expand_dims(obs, axis=0)
-        action = agent.predict(batch_obs.astype('float32'))
-        action = np.squeeze(action)
-
-        # 给输出动作增加探索扰动，输出限制在 [-1.0, 1.0] 范围内
-        action = np.random.normal(action, 1.0)
-        action = np.clip(action, -1.0, 1.0)
-        # 动作映射到对应的 实际动作取值范围 内, action_mapping是从parl.utils那里import进来的函数
-        action = action_mapping(action, env.action_space.low[0],
-                                env.action_space.high[0])
-
-        next_obs, reward, done, info = env.step(action)
-        rpm.append(obs, action, REWARD_SCALE * reward, next_obs, done)
-
-        if rpm.size() > MEMORY_WARMUP_SIZE:
-            batch_obs, batch_action, batch_reward, batch_next_obs, \
-                    batch_terminal = rpm.sample_batch(BATCH_SIZE)
-            critic_cost = agent.learn(batch_obs, batch_action, batch_reward,
-                                      batch_next_obs, batch_terminal)
-
-        obs = next_obs
-        total_reward += reward
-
-        if fff>500:
-            print("obs",obs[0:10],reward)
-            break
-    return total_reward, steps
-
-# 评估 agent, 跑 5 个episode，总reward求平均
-def evaluate(env, agent):
-    ff=0
-    eval_reward = []
-    for i in range(5):
-        obs = env.reset()
-        total_reward, steps = 0, 0
-        while True:
-            batch_obs = np.expand_dims(obs, axis=0)
-            action = agent.predict(batch_obs.astype('float32'))
-            action = np.squeeze(action)
-            action = np.clip(action, -1.0, 1.0)
-            action = action_mapping(action, env.action_space.low[0], 
-                                    env.action_space.high[0])
-
-            next_obs, reward, done, info = env.step(action)
-            ff +=1
-
-            obs = next_obs
-            total_reward += reward
-            steps += 1
-            
-            if ff>3500:
-                print("end with " ,reward)
-                break
-        eval_reward.append(total_reward)
-    return np.mean(eval_reward)
-
-font = fm.FontProperties(fname='font/wqy-microhei.ttc')
-# plt.rc('font', family='Source Han Sans CN')
-plt.rcParams['axes.unicode_minus'] = False
 
 
 
-
-# 创建环境
 
 act_dim = env.action_space.shape[0]
 obs_dim = env.observation_space.shape[0] 
@@ -270,31 +201,44 @@ model = QuadrotorModel(act_dim=act_dim)
 algorithm = DDPG(
     model, gamma=GAMMA, tau=TAU, actor_lr=ACTOR_LR, critic_lr=CRITIC_LR)
 agent = Agent(algorithm, obs_dim=obs_dim, act_dim=act_dim)
+total_reward, steps = 0, 0
+EPISODES = 500
+TAU = 0.01
+NUM_OF_SELLER=5
+N_ACTIONS = NUM_OF_SELLER*2
+NUM_OF_BUYER=9
+result = np.zeros((EPISODES, N_ACTIONS + 1 + 1))
+for i in range(EPISODES):
+    obs = env.reset()
+    np.random.seed(0)
+    score_history = []
+    print(i)
+    print(" episode start with" + str(["%.2f" % val for val in obs[0:NUM_OF_SELLER * 2]]))
+    while True:
+        steps += 1
+        batch_obs = np.expand_dims(obs, axis=0)
+        action = agent.predict(batch_obs.astype('float32'))
+        action = np.squeeze(action)
 
-# 加载模型
-# save_path = './dqn_model.ckpt'
-# agent.restore(save_path)
+        # 给输出动作增加探索扰动，输出限制在 [-1.0, 1.0] 范围内
+        action = np.random.normal(action, 1.0)
+        action = np.clip(action, -1.0, 1.0)
+        action = action_mapping(action,env.action_space.low[0],env.action_space.high[0])
+        # 动作映射到对应的 实际动作取值范围 内, action_mapping是从parl.utils那里import进来的函数
 
-# 先往经验池里存一些数据，避免最开始训练的时候样本丰富度不够
+        next_obs, reward, done, info = env.step(action)
 
-max_episode = 500
-test_flag = 0
-total_steps = 0
-while total_steps < TRAIN_TOTAL_STEPS:
-    train_reward, steps = run_episode(env, agent, rpm)
-    total_steps += steps
-    #logger.info('Steps: {} Reward: {}'.format(total_steps, train_reward)) # 打印训练reward
+        rpm.append(obs, action, REWARD_SCALE * reward, next_obs, done)
 
-    if total_steps // TEST_EVERY_STEPS >= test_flag: # 每隔一定step数，评估一次模型
-        while total_steps // TEST_EVERY_STEPS >= test_flag:
-            test_flag += 1
+        if rpm.size() > MEMORY_WARMUP_SIZE:
+            batch_obs, batch_action, batch_reward, batch_next_obs, \
+            batch_terminal = rpm.sample_batch(BATCH_SIZE)
+            critic_cost = agent.learn(batch_obs, batch_action, batch_reward,
+                                      batch_next_obs, batch_terminal)
+        obs = next_obs
+        total_reward += reward
+        if steps > 300:
+            result[i] = np.concatenate((next_obs[0:NUM_OF_SELLER * 2], [reward, np.mean(score_history[-50:])]))
+            print("episode end with  " + str(["%.2f" % val for val in next_obs[0:NUM_OF_SELLER * 2]]))
+            break
 
-        evaluate_reward = evaluate(env, agent)
-        print(test_flag,evaluate_reward)
-        env.render()
-        logger.info('Steps {}, Test reward: {}'.format(
-            total_steps,evaluate_reward)) # 打印评估的reward
-
-        # 每评估一次，就保存一次模型，以训练的step数命名
-        ckpt = 'steps_{}.ckpt'.format(total_steps)
-        agent.save(ckpt)
